dc55a1d0769809025be33c6994f62d954a3b3bd4
tangtangsama <1462339680@qq.com>
2020-08-31 15:40:05 +0800

    实现Dao的实现类，使用MyBatis（无必要）

---
+++ src/main/java/cn/sucrelt/dao/impl/UserDaoImpl.java
@@ -0,0 +1,37 @@
+package cn.sucrelt.dao.impl;
+
+import cn.sucrelt.dao.UserDao;
+import cn.sucrelt.domain.User;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import java.util.List;
+
+/**
+ * @description: 实现UserDao类，通过实现Dao类可以不通过SqlSession实例获取到实体类，但是没有必要，这样会增加需要编写的代码量
+ * @author: sucre
+ * @date: 2020/08/31
+ * @time: 15:00
+ */
+public class UserDaoImpl implements UserDao {
+
+    private SqlSessionFactory factory;
+
+    /**
+     * 覆盖默认构造函数，保证该factory对象有值
+     *
+     * @param factory
+     */
+    public UserDaoImpl(SqlSessionFactory factory) {
+        this.factory = factory;
+    }
+
+    public List<User> findAll() {
+        //使用factory创建Session对象并执行查询方法
+        SqlSession session = factory.openSession();
+        List<User> users = session.selectList("cn.sucrelt.dao.UserDao.findAll");
+
+        session.close();
+        return users;
+    }
+}
---
+++ src/test/java/cn/sucrelt/test/MybatisTest2.java
@@ -0,0 +1,42 @@
+package cn.sucrelt.test;
+
+import cn.sucrelt.dao.UserDao;
+import cn.sucrelt.dao.impl.UserDaoImpl;
+import cn.sucrelt.domain.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * @description:
+ * @author: sucre
+ * @date: 2020/08/31
+ * @time: 15:02
+ */
+public class MybatisTest2 {
+    /**
+     * 入门案例,使用Dao的实现类创建对象并使用MyBatis
+     * @param args
+     */
+    public static void main(String[] args) throws IOException {
+        //1.读取配置文件
+        InputStream in = Resources.getResourceAsStream("SqlMapConfig.xml");
+        //2.创建SqlSessionFactory工厂
+        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+        SqlSessionFactory factory = builder.build(in);
+        //3.使用实现类创建Dao对象
+        UserDao userDao = new UserDaoImpl(factory);
+        //4.使用代理对象执行方法
+        List<User> users = userDao.findAll();
+        for (User user :
+                users) {
+            System.out.println(user);
+        }
+        //5.释放资源
+        in.close();
+    }
+}
